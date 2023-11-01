2231117b4acf0eecc9f1bbbc5c743c1779458e5c
moninn <1moninnly@gmail.com>
2023-11-01 21:00:11 +0100

    s

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Mode nuit */
+
 const modeNuitToggle = document.getElementById('modeNuitToggle');
 const iconeJour = document.getElementById('iconeJour');
 const iconeNuit = document.getElementById('iconeNuit');
@@ -36,3 +38,57 @@ modeNuitToggle.addEventListener('click', () => {
     activerModeNuit();
   }
 });
+
+/* Slider */
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const indicators = document.querySelectorAll('.indicator');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+function updateIndicators(index) {
+  indicators.forEach((indicator, i) => {
+    if (i === index) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
+}
+
+function prevSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide(currentSlide);
+  updateIndicators(currentSlide);
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+  updateIndicators(currentSlide);
+}
+
+function goToSlide(index) {
+  currentSlide = index;
+  showSlide(currentSlide);
+  updateIndicators(currentSlide);
+}
+
+showSlide(currentSlide);
+updateIndicators(currentSlide);
+
